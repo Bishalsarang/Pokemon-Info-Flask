e7a2d195002b999bd48d504cf17f9e19b3ef3fe9
sarangbishal <sarangbishal@gmail.com>
2020-01-04 21:54:15 +0545

    Make template working\

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 # Author: Bishal Sarang
 
-from flask import Flask, render_template, request, flash, json
+from flask import Flask, render_template, request, flash, redirect
 from models import load_data
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    return "Welcome"
 
 
 @app.route('/pokedox/<string:pokemon_name>')
@@ -16,14 +17,17 @@ def pokedox(pokemon_name):
     all_pokemon_data = load_data()['pokemons']
 
     requested_pokemon_data = all_pokemon_data.get(pokemon_name)
-    requested_pokemon_description = requested_pokemon_data["description"]
-    requested_pokemon_info = requested_pokemon_data["info"]
 
-    requested_pokemon_type = requested_pokemon_data.get("type")
-    requested_pokemon_weakness = requested_pokemon_data.get("weakness")
+    if requested_pokemon_data is not None:
+        pokemon_description = requested_pokemon_data["description"]
+        pokemon_info = requested_pokemon_data["info"]
 
-    return   render_template("index.html", pokemon_name=pokemon_name, requested_pokemon_description=requested_pokemon_description)
+        pokemon_type = pokemon_info.get("type")
+        pokemon_abilities = pokemon_info.get("abilities")
+        pokemon_weakness = pokemon_info.get("weakness")
 
+        return  render_template("index.html", pokemon_name=pokemon_name, pokemon_description=pokemon_description, pokemon_type=pokemon_type, pokemon_abilities=pokemon_abilities, pokemon_weakness=pokemon_weakness)
+    return "Not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
